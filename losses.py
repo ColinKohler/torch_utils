6dda3f52035fec07bb0479899927cbb1c55c9539
Colin Kohler <colink78@gmail.com>
2021-03-25 18:29:14 -0400

    Small bug fix due to misnamed function.

--- losses.py
+++ losses.py
@@ -1,19 +1,19 @@
 import torch
 
-def scalarToSupport(x, support_size):
+def scalarToSupport(x, support_size, eps=1):
   '''
   Transform a scalar to a categorical representation.
   Number of categories = 2 * support_size + 1
   '''
   # Reduce the scale (https://arxiv.org/abs/1805.11593)
-  x = torch.sign(x) * (torch.sqrt(torch.abs(x) + 1) - 1) + 1e-3
+  x = torch.sign(x) * (torch.sqrt(torch.abs(x) + 1) - 1 + eps * x)
 
   # Encode vector
   x = torch.clamp(x, -support_size, support_size)
   floor = x.floor()
   prob = x - floor
 
-  logits = torch.zeros(x.size(0), x.size(1), 2 * support_size + 1).to(x.device)
+  logits = torch.zeros(x.shape[0], x.shape[1], 2 * support_size + 1).to(x.device)
   logits.scatter_(2, (floor + support_size).long().unsqueeze(-1), (1 - prob).unsqueeze(-1))
 
   idxs = floor + support_size + 1
@@ -23,7 +23,7 @@ def scalarToSupport(x, support_size):
 
   return logits
 
-def supportToScalar(logits, support_size):
+def supportToScalar(logits, support_size, eps=1):
   '''
   Transform a categorical representation to a scalar.
   '''
@@ -38,9 +38,9 @@ def supportToScalar(logits, support_size):
 
   # Invert the scaling (https://arxiv.org/abs/1805.11593)
   x = torch.sign(x) * (
-    ((torch.sqrt(1 + 4 * 1e-3 * (torch.abs(x) + 1 + 1e-3)) - 1) / (2 * 1e-3))
+    ((torch.sqrt(1 + 4 * eps * (torch.abs(x) + 1 + eps)) - 1) / (2 * eps))
     **2
-    -1
+    - 1
   )
 
   return x
--- utils.py
+++ utils.py
@@ -43,7 +43,7 @@ def dictToCpu(dictionary):
     if isinstance(value, torch.Tensor):
       cpu_dict[key] = value.cpu()
     elif isinstance(value, dict):
-      cpu_dict[key] = dict_to_cpu(value)
+      cpu_dict[key] = dictToCpu(value)
     else:
       cpu_dict[key] = value
   return cpu_dict
