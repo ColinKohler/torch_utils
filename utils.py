4c6ff5d1e1c7bc241cf4d80d09d27df38e15c7e8
Colin Kohler <colink78@gmail.com>
2021-07-07 15:35:54 -0400

    Fixing torch floor deprication by using divide function and setting the rounding mode as appropriate

--- utils.py
+++ utils.py
@@ -21,20 +21,20 @@ def argmax2d(tensor):
   n = tensor.size(0)
   d = tensor.size(-1)
   idx = tensor.view(n, -1).argmax(1)
-  return torch.cat(((idx // d).view(-1, 1), (idx % d).view(-1, 1)), dim=1)
+  return torch.cat((torch.divide(idx, d, rounding_mode='trunc').view(-1, 1), (idx % d).view(-1, 1)), dim=1)
 
 def topk2d(tensor, k=1):
   n = tensor.size(0)
   d = tensor.size(-1)
   val, idx = tensor.view(n, -1).topk(k)
-  idx = torch.cat(((idx // d).view(-1, 1), (idx % d).view(-1, 1)), dim=1)
+  idx = torch.cat((torch.divide(idx, d, rounding_mode='trunc').view(-1, 1), (idx % d).view(-1, 1)), dim=1)
   return val, idx
 
 def sample2d(tensor, k=1):
   n = tensor.size(0)
   d = tensor.size(-1)
   idx =  torch.multinomial(tensor.view(n, -1), k)
-  return torch.cat(((idx // d).view(-1, 1), (idx % d).view(-1, 1)), dim=1)
+  return torch.cat((torch.divide(idx, d, rounding_mode='trunc').view(-1, 1), (idx % d).view(-1, 1)), dim=1)
 
 def softUpdate(target_net, source_net, tau):
   for target_param, source_param in zip(target_net.parameters(), source_net.parameters()):
